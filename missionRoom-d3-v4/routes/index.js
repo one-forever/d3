8290fe52b9dd5ecfa36491acd60f78872b57a9b9
zxy <1178794332@qq.com>
2017-09-30 18:12:11 +0800

    d3 tree图

---
+++ missionRoom-d3-v4/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'D3' });
+});
+
+router.get('/v4', function(req, res, next) {
+    res.render('index-v4', { title: 'D3' });
+});
+module.exports = router;
